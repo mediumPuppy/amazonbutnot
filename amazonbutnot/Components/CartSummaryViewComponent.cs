6980f97bb66c512702c0e9ae3c9dbf4a37940b11
n8shaw <nathanjshaw88@gmail.com>
2024-04-10 12:06:13 -0600

    added cart vc

---
+++ amazonbutnot/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using amazonbutnot.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace amazonbutnot.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+
+
+    }
+}
