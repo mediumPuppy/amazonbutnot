2b2e58a4bb8196f5f2bab7d4a9b150f27dd23965
Ben Macmichael <benjaminmac21@gmail.com>
2024-04-09 19:05:45 -0600

    connected to azure identity db

--- amazonbutnot/Program.cs
+++ amazonbutnot/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Product DB setup
